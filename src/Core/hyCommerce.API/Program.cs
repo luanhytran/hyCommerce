51a587433d46b97740c84a2f429330fb447ae618
luanhytran <hytranluan@gmail.com>
2025-05-14 19:51:30 +0700

    Group services in hyCommerce.API.csproj to DependencyInjection.cs in hyCommerce.Application.csproj

--- src/Core/hyCommerce.API/Program.cs
+++ src/Core/hyCommerce.API/Program.cs
@@ -1,92 +1,22 @@
-using hyCommerce.Application.Services;
-using hyCommerce.Domain.Entities;
+using hyCommerce.Application;
 using hyCommerce.Infrastructure.Persistence;
 using hyCommerce.Infrastructure.Persistence.Data;
-using hyCommerce.Infrastructure.Persistence.Repositories;
 using hyCommerce.Notification;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.IdentityModel.Tokens;
-using Microsoft.OpenApi.Models;
-using System.Text;
-using hyCommerce.Domain;
-using hyCommerce.Domain.Repositories;
 using hyCommerce.EventBus;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddControllers();
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen(c =>
-{
-    // Include 'SecurityScheme' to use JWT Authentication
-    var jwtSecurityScheme = new OpenApiSecurityScheme
-    {
-        BearerFormat = "JWT",
-        Name = "Authorization",
-        In = ParameterLocation.Header,
-        Type = SecuritySchemeType.ApiKey,
-        Scheme = JwtBearerDefaults.AuthenticationScheme,
-        Description = "Put Bearer + your token in the box below",
+builder.Services.AddApplicationServices(builder.Configuration);
 
-        Reference = new OpenApiReference
-        {
-            Id = JwtBearerDefaults.AuthenticationScheme,
-            Type = ReferenceType.SecurityScheme
-        }
-    };
-
-    c.AddSecurityDefinition(jwtSecurityScheme.Reference.Id, jwtSecurityScheme);
-
-    c.AddSecurityRequirement(new OpenApiSecurityRequirement
-    {
-        { jwtSecurityScheme, Array.Empty<string>() }
-    });
-});
-
-builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-    .AddJwtBearer(opt =>
-    {
-        opt.TokenValidationParameters = new TokenValidationParameters
-        {
-            ValidateIssuer = false,
-            ValidateAudience = false,
-            ValidateLifetime = true,
-            ValidateIssuerSigningKey = true,
-            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8
-                .GetBytes(builder.Configuration["JWTSettings:TokenKey"])),
-            ClockSkew = TimeSpan.Zero
-        };
-    });
-builder.Services.AddAuthorization();
-
-builder.Services.AddIdentityCore<User>(opt =>
-    {
-        opt.User.RequireUniqueEmail = true;
-        opt.Password.RequireNonAlphanumeric = false;
-        opt.SignIn.RequireConfirmedEmail = true;
-    })
-    .AddUserManager<ApplicationUserManager>()
-    .AddRoles<Role>()
-    .AddEntityFrameworkStores<AppDbContext>();
+builder.Services.AddIdentity<AppDbContext>(builder.Configuration, builder.Environment);
 
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("Docker_Postgres")));
 
-builder.Services.AddHttpContextAccessor();
-
 builder.Services.AddMailSender(builder.Configuration);
 
 builder.Services.AddCap<AppDbContext>(builder.Configuration);
-
-builder.Services.AddScoped<IIdentityService, IdentityService>();
-builder.Services.AddScoped<IProductService, ProductService>();
-builder.Services.AddScoped<IProductRepository, ProductRepository>();
-builder.Services.AddScoped<ITokenService, TokenService>();
-builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
 
 var app = builder.Build();
 
@@ -100,6 +30,7 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 
 app.UseAuthentication();
+
 app.UseAuthorization();
 
 app.MapControllers();
